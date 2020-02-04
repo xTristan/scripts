e7c060dd411f060df5a1c1847f6a049481bddc16
Jian Lu <tristan.uva@gmail.com>
2020-02-03 20:03:37 -0800

    Update init_os.sh

--- shell/init_os.sh
+++ shell/init_os.sh
@@ -32,9 +32,12 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 # echo 'source  /usr/share/powerlevel9k/powerlevel9k.zsh-theme' >> ~/.zshrc
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 ### Install Nerd Fonts
-git clone https://github.com/ryanoasis/nerd-fonts.git ~/backups/
-cd ~/backups/nerd-fonts
+# git clone https://github.com/ryanoasis/nerd-fonts.git ~/backups/
+# cd ~/backups/nerd-fonts
+mkdir -p ~/.local/share/fonts
 
+cd ~/.local/share/fonts && curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20Nerd%20Font%20Complete.otf
+fc-cache -fv
 
 # copy dotfiles
 ## make backups of vanila files
