85feb56492d4830b863248a2140127c9fb0f863f
Jian Lu <tristan.uva@gmail.com>
2020-09-16 23:54:43 -0700

    Update init_os.sh

--- shell/init_os.sh
+++ shell/init_os.sh
@@ -77,7 +77,20 @@ git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shel
 # Manual install: https://github.com/Airblader/i3/wiki/Building-from-source
 # Install dependencies:
 # sudo apt install dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+# clone the repository
+# git clone https://www.github.com/Airblader/i3 i3-gaps
+# cd i3-gaps
 
+# compile & install
+# autoreconf --force --install
+# rm -rf build/
+# mkdir -p build && cd build/
+
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+# ../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+# make
+# sudo make install
 
 # install polybar, does not work on debian, needs manual install.
 sudo apt install polybar
