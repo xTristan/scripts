e664aa6cff115f5ee3e957d6a427e63def5d27f9
Jian Lu <tristan.uva@gmail.com>
2021-03-09 22:25:00 -0800

    Update init_os.sh

--- shell/init_os.sh
+++ shell/init_os.sh
@@ -64,6 +64,12 @@ ln -s $HOME/dotfiles/.Xresources $HOME/.Xresources
 ## Install Base16 scheme
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
+## Install i3-wm
+sudo apt install i3-wm
+sudo apt install libev-dev libstartup-notification0-dev dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+
+
+
 # install i3, does not work on debian, needs manual install.
 # sudo add-apt-repository ppa:kgilmer/speed-ricer
 # sudo apt-get update
