1b4a4c99477443e9dc2d93a5971db7456739b426
Jian Lu <tristan.uva@gmail.com>
2021-03-30 00:16:39 -0700

    support for switching between multiple monitors.

--- shell/i3_multi_monitors.sh
+++ shell/i3_multi_monitors.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 # toggles the external monitor on/off in specified direction
-INTERNAL="eDP-1"
-EXTERNAL="DP-1-2"
+INTERNAL="eDP1"
+EXTERNAL_DELL="DP1"
+EXTERNAL_OTHER="HDMI-1"
 DIRECTION="above"
 
-if (xrandr | grep "$EXTERNAL connected"); then
-    xrandr --output $INTERNAL --mode 2560x1440
-    xrandr --output $EXTERNAL --primary --scale 1x1 --mode 3440x1440 --$DIRECTION $INTERNAL
+if (xrandr | grep " $EXTERNAL_DELL connected"); then
+   xrandr --output $INTERNAL --scale 1x1 --mode 2560x1440
+   xrandr --output $EXTERNAL_DELL --primary --scale 1.5x1.5 --mode 3440x1440 --$DIRECTION $INTERNAL
+elif (xrandr | grep " $EXTERNAL_OTHER connected"); then
+   xrandr --output $INTERNAL --scale 1x1 --mode 2560x1440
+   xrandr --output $EXTERNAL_OTHER --primary --scale 1x1 --mode 2560x1440 --$DIRECTION $INTERNAL
 else
-    xrandr --output $INTERNAL --primary --mode 2560x1440
-    xrandr --output $EXTERNAL --off 
+    xrandr --output $INTERNAL --primary --scale 0.9x0.9 --mode 2560x1440
+    xrandr --output $EXTERNAL_DELL --off 
+    xrandr --output $EXTERNAL_OTHER --off 
 fi
