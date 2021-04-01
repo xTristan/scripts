c2d20fc2383c166e63dc30b5f7eeada747f0a6fa
Jian Lu <tristan.uva@gmail.com>
2021-03-31 18:06:21 -0700

    set up HDMI

--- shell/setup_external_monitors.sh
+++ shell/setup_external_monitors.sh
@@ -6,10 +6,12 @@
 INTERNAL=`xrandr | grep -oP "(?=^)eDP-*1(?= connected)"`
 EXTERNAL_DP=`xrandr | grep -oP "(?=^)DP-*1(?= connected)"`
 EXTERNAL_HDMI=`xrandr | grep -oP "(?=^)HDMI-*1(?= connected)"`
+DIRECTION='above'
 
 echo $INTERNAL
 echo $EXTERNAL_DP
 echo $EXTERNAL_HDMI
+echo $DIRECTION
 
 if [ -n "$EXTERNAL_DP" ]; then
   echo "$EXTERNAL_DP connected as external monitor. Setting it as primary."
@@ -18,9 +20,9 @@ if [ -n "$EXTERNAL_DP" ]; then
   xrandr --output $INTERNAL --scale 1x1 --mode 2560x1440
 elif [ -n "$EXTERNAL_HDMI" ]; then
   echo "$EXTERNAL_HDMI connected as external monitor. Setting it as primary."
-  xrandr --output $EXTERNAL_HDMI --primary --scale 1x1 --mode 3440x1440 --$DIRECTION $INTERNAL
-  echo "Setting up laptop monitor as secondary with no scaling."
-  xrandr --output $INTERNAL --scale 1x1 --mode 2560x1440
+  xrandr --output $EXTERNAL_HDMI --primary --scale 1x1 --mode 2560x1440 --$DIRECTION $INTERNAL
+  echo "Setting up laptop monitor as secondary to 2560x1440 with 0.9 scaling (slightly largr font)."
+  xrandr --output $INTERNAL --scale 0.9x0.9 --mode 2560x1440
 else
   echo "No external monitor connected"
   echo "Setting up laptop monitor as primiary to 2560x1440 with 0.9 scaling (slightly largr font)."
