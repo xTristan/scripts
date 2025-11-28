d322bda2792d9c9e1f3666e2f0e46c9f41341b69
Jian Lu <tristan.uva@gmail.com>
2025-11-28 15:35:05 -0800

    Enable long paths and update installation script
    Enable long path support and add comments for clarity.

--- shell/init_windows.ps1
+++ shell/init_windows.ps1
@@ -1,5 +1,10 @@
 winget source update
 winget upgrade Microsoft.PowerShell
+Set-ItemProperty 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1
 winget install LGUG2Z.komorebi LGUG2Z.whkd 
+# Configure komorebi and whkd
 winget install Git.Git
+
+# oh-my-posh
 winget install JanDeDobbeleer.OhMyPosh --source winget
+# Install NerdFonts
